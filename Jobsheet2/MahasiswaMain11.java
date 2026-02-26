f724583ae62ac8e628bca00cfd70c3beee467f6f
galangsatriyo <mnuriromadhon2@gmail.com>
2026-02-26 15:57:28 +0700

    Percobaan 3

--- Jobsheet2/MahasiswaMain11.java
+++ Jobsheet2/MahasiswaMain11.java
@@ -12,5 +12,9 @@ public class MahasiswaMain11 {
         mhsl.ubahKelas("SI 2K");
         mhsl.updateIpk(3.60);
         mhsl.tampilkanInformasi();
+
+        Mahasiswa11 mhs2 = new Mahasiswa11("Annisa Nabila" , "2141720160", 3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
