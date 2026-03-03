da7fa3e83b121560d724aeea0dfcd0c539b9755b
galangsatriyo <mnuriromadhon2@gmail.com>
2026-03-04 00:20:36 +0700

    Percobaan 1

---
+++ jobsheet3/MahasiswaDemo11.java
@@ -0,0 +1,32 @@
+package jobsheet3;
+
+import java.util.Scanner;
+
+public class MahasiswaDemo11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Mahasiswa11[] arrayOfMahasiswa = new Mahasiswa11[3];
+        String dummy;
+       
+        for(int i=0 ; i < 3; i++) {
+            arrayOfMahasiswa[i] = new Mahasiswa11();
+
+            System.out.println("Masukan Data Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM     : " );
+            arrayOfMahasiswa[i].nim = sc.nextLine();
+            System.out.print("Nama    : ");
+            arrayOfMahasiswa[i].nama = sc.nextLine();
+            System.out.print("Kelas   : ");
+            arrayOfMahasiswa[i].kelas = sc.nextLine();
+            System.out.print("IPK     : ");
+            dummy = sc.nextLine();
+            arrayOfMahasiswa[i].ipk = Float.parseFloat(dummy);
+            System.out.println("-----------------------------------------");
+        }
+        for(int i=0; i < 3; i++) {
+            System.out.println("Data Mahasiswa ke-" + (i + 1));
+            arrayOfMahasiswa[i].cetakInfo();
+        }
+    } 
+}
+
